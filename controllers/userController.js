f54c5557e9872c08c4a98bd7b80da1d3f2a1f44c
jeevakalaiselvam <jeevak001@outlook.com>
2021-11-15 21:51:32 +0530

    Refactor

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require('../config/database');
+const { TOURME_ROLES } = require('../constants/tourmeConstants');
 const TourmeProject = require('../models/TourmeProject');
 const TourmeUser = require('../models/TourmeUser');
 
@@ -24,119 +25,45 @@ exports.getAllUserDetails = async (req, res) => {
   }
 };
 
-//Get all Admin Users for Tourme
+//Get all User Details, If not present, Onboard User and Project
 exports.getUserDetails = async (req, res) => {
   try {
-    const { userId } = req.body;
-    console.log('JEEVA', userId);
+    const { userId, emailId, fullName, projectName } = req.body;
     const tourmeUser = await TourmeUser.findOne({ where: { userId: userId } });
-    const projectsForUser = await TourmeProject.findAll({
-      where: { userId: userId },
-    });
-    Promise.all([tourmeUser, projectsForUser])
-      .then((response) => {
-        if (response[0]) {
-          //User is present
-          if (response[1]) {
-            //User has Projects
+
+    if (tourmeUser !== null) {
+      const projectsForUser = await TourmeProject.findAll({
+        where: { userId: userId },
+      });
+
+      if (projectsForUser !== null) {
+        res.status(200).json({
+          status: 'success',
+          tourmeUser,
+          projectsForUser,
+        });
+      }
+    } else {
+      const newUser = TourmeUser.create({ userId, emailId, fullName });
+      if (newUser !== null) {
+        const newProject = TourmeProject.create({
+          userId,
+          projectName,
+          roleType: TOURME_ROLES.TOURME_ADMIN,
+        });
+
+        if (newProject !== null) {
+          const projectsForUser = await TourmeProject.findAll({
+            where: { userId: userId },
+          });
+          if (projectsForUser) {
             res.status(200).json({
               status: 'success',
-              tourmeUser: response[0],
-              projectsForUser: response[1],
-            });
-          } else {
-            res.status(200).json({
-              status: 'success',
-              tourmeUser: response[0],
-              projectsForUser: {},
+              newUser,
+              projectsForUser,
             });
           }
-        } else {
-          console.log('USER NOT PRESENT');
-          res.status(200).json({
-            tourmeUser: {},
-            projectsForUser: {},
-            status: 'success',
-          });
         }
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({
-          status: 'error',
-          error,
-        });
-      });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      status: 'error',
-    });
-  }
-};
-
-//Add a Admin User to Tourme
-exports.addNewUserDetails = async (req, res) => {
-  const { userId, fullName, emailId } = req.body;
-
-  try {
-    const user = await TourmeUser.create({
-      userId,
-      fullName,
-      emailId,
-    });
-
-    if (user) {
-      res.status(201).json({
-        status: 'success',
-        user,
-      });
-    } else {
-      res.status(201).json({
-        status: 'fail',
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      status: 'error',
-    });
-  }
-};
-
-//Onboard User and Project
-exports.onboardProject = async (req, res) => {
-  try {
-    const { userId, emailId, fullName, projectName, roleType } = req.body;
-    console.log({ userId, emailId, fullName, projectName, roleType });
-    console.log('Onboarding', req.body);
-
-    const user = await TourmeUser.create({
-      userId,
-      emailId,
-      fullName,
-    });
-
-    console.log(user);
-
-    if (user !== null) {
-      const project = await TourmeProject.create({
-        userId,
-        projectName,
-        roleType,
-      });
-
-      if (project !== null) {
-        console.log('Both Onboarded..');
-        res.status(201).json({
-          status: 'success',
-          project,
-          user,
-        });
-      } else {
-        res.status(200).json({
-          status: 'fail',
-        });
       }
     }
   } catch (error) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,5 @@ const router = express.Router();
 
 router.post('/all', userController.getAllUserDetails);
 router.post('/info', userController.getUserDetails);
-router.post('/new', userController.addNewUserDetails);
-router.post('/onboard', userController.onboardProject);
 
 module.exports = router;
