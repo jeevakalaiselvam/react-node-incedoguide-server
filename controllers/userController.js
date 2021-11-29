4906f751fcda586c594a43ef3781fd15ccfb41b0
jeevakalaiselvam <jeevak001@outlook.com>
2021-11-29 15:22:14 +0530

    CurrentUserId and Roles added

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,8 @@ exports.getAllUserDetails = async (req, res) => {
 //Get all User Details, If not present, Onboard User and Project
 exports.getUserDetails = async (req, res) => {
   try {
-    const { userId, emailId, fullName, projectName } = req.body;
+    const { userId, emailId, fullName, projectName, projectRoles } = req.body;
+    console.log('PROJECT ROLES', projectRoles);
     const user = await User.findOne({ where: { userId: userId } });
 
     if (user !== null) {
@@ -46,10 +47,19 @@ exports.getUserDetails = async (req, res) => {
     } else {
       const newUser = await User.create({ userId, emailId, fullName });
       if (newUser !== null) {
+        //For user provided roles, Create mapping with default number of users in that role
+        let newProjectRoles = {};
+        //Add configuring user as default admin and add other roles provided in config
+        newProjectRoles = { ...newProjectRoles, MAIN_ADMIN: [userId] };
+        projectRoles.forEach((role) => {
+          newProjectRoles = { ...newProjectRoles, [role]: [] };
+        });
+
         const newProject = await Project.create({
           userId,
           projectName,
           roleType: INCEDOGUIDE_ROLES.INCEDOGUIDE_ADMIN,
+          projectRoles: newProjectRoles,
         });
 
         if (newProject !== null) {
